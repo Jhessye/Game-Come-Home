f44c3235b34884a34cadf14c43d7ccd95e61dfd4
Jhessye Lorrayne <122307033+Jhessye@users.noreply.github.com>
2026-01-19 00:25:47 -0300

    melhoras e alterações de organização
    sound efects

---
+++ game-come-home/script/botoes/button_creditos.gd
@@ -0,0 +1,21 @@
+extends Button
+
+@export var next_scene: String = "res://cenas/tela inicial/title_screem.tscn"
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	audio_stream_player.stop()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file(next_scene)
+
+
+func _on_mouse_entered() -> void:
+	audio_stream_player.stop()
+	audio_stream_player.play()
---
+++ game-come-home/script/botoes/check_button.gd
@@ -0,0 +1,41 @@
+extends CheckButton
+
+@export var next_scene: String = "res://cenas/jogaveis/cena_.tscn"
+@export var delay_seconds: int = 3
+
+@onready var countdown_label: Label = $Label
+
+var _transition_id := 0
+
+
+func _ready():
+	countdown_label.text = ""
+
+
+func _toggled(button_pressed: bool) -> void:
+	if button_pressed:
+		start_transition()
+	else:
+		cancel_transition()
+
+
+func start_transition():
+	_transition_id += 1
+	var my_id = _transition_id
+
+	for i in range(delay_seconds, 0, -1):
+		# se foi cancelado, para tudo
+		if my_id != _transition_id or not button_pressed:
+			return
+
+		countdown_label.text = str(i)
+		await get_tree().create_timer(1.0).timeout
+
+	# troca de cena se ainda estiver válido
+	if my_id == _transition_id and button_pressed:
+		get_tree().change_scene_to_file(next_scene)
+
+
+func cancel_transition():
+	_transition_id += 1
+	countdown_label.text = ""
