0c4daaafd44b93b302c3cb868c0583135406c017
Jhessye Lorrayne <122307033+Jhessye@users.noreply.github.com>
2026-01-20 22:22:00 -0300

    JESUS QUE LUTAAAAAAAAAA

--- game-come-home/script/title_screem.gd
+++ game-come-home/script/title_screem.gd
@@ -3,10 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Inventory.set_item(0, "espada")
+	Inventory.set_item(1, "escudo")
+	Inventory.set_item(2, "mapa")
 	GlobalAudioStreamPlayer.play_music_level()
-	pass # Replace with function body.
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
